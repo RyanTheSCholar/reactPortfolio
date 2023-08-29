f8d43c8be18f6c173afa1ddf39e4d3cb7409129a
RyanTheScholar <Ryan.business.bowen@gmail.com>
2023-08-29 10:20:41 -0500

    added started code

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from './App.jsx'
+import './index.css'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    // add an optional error page
+    children: [
+      {
+        index: true,
+        // element: <About />
+      },
+      {
+        path: '/Portfolio',
+        // element: < Portfolio/>
+      },
+      {
+        path: '/Contact',
+        // element: <Contact/>
+      },
+      {
+        path: '/Resume',
+        // element: < Resume/>
+      }
+
+    ],
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
